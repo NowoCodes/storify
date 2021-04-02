79f7f0b933d65ca9163fd169a46362969778be1c
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-02 11:13:47 +0100

    define database relationship

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Story extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'type',
+        'status'
+    ];
+
+    // protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
