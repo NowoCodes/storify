585cacc4c9c11680fc2f4ada34388572b5930698
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-02 13:43:12 +0100

    display stories based on user

--- app/Http/Controllers/StoriesController.php
+++ app/Http/Controllers/StoriesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Story;
 
 class StoriesController extends Controller
 {
-    //
+    public function index() {
+        $stories = Story::where('user_id', auth()->user()->id)
+            ->orderBy('id', 'DESC');
+            // ->simplePaginate(5);
+        return view('stories.index', [
+            'stories' => $stories,
+        ]);
+    }
 }
