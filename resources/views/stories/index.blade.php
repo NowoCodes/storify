10b3103fb1e4c0f8d6e0070b39ab52768172070a
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-02 11:41:38 +0100

    add bootstrap files

---
+++ resources/views/stories/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.home')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-cotent-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">Stories</div>
+
+          <div class="card-body">
+            Story
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
