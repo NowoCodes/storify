15be395a4b3d9fe66a28533e2b12c3eaf209081b
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-05 23:44:36 +0100

    Create create.blade.php

---
+++ resources/views/stories/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.home')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-cotent-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            Add Story
+
+            <a class="float-right" href="{{ route('stories.index') }}">Back</a>
+          </div>
+
+          <div class="card-body">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
