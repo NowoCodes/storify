8f26415b4c937214fb751b7fa1c0945412388d7e
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-06 00:33:12 +0100

    add alert message on form submit

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="stylesheet" href="{{ asset('dist/css/bootstrap.css') }}">
-        <link rel="stylesheet" href="{{ asset('dist/js/bootstrap.js') }}">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('dist/css/bootstrap.min.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-            <!-- Page Content -->
-            <main class="container pt-5">
-                @yield('content')
-            </main>
-    </body>
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans antialiased">
+  <!-- Page Content -->
+  <main class="container pt-5">
+    @if (session('status'))
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        {{ session('status') }}
+      </div>
+    @endif
+    @yield('content')
+  </main>
+
+
+  <script src="{{ asset('dist/js/bootstrap.min.js') }}"></script>
+</body>
+
 </html>
