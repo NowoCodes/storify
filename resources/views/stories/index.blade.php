be6bc4bd54dfbba45cabd9507faa8c4226036c95
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-06 17:39:26 +0100

    Update index.blade.php

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -5,36 +5,39 @@
     <div class="row justify-cotent-center">
       <div class="col-md-12">
         <div class="card">
-          <div class="card-header">Stories</div>
+          <div class="card-header">
+            Stories
+            <a href="{{ route('stories.create') }}" class="float-right">Add Story</a>
+          </div>
 
           <div class="card-body">
             <table class="table table-sm">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Type</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($stories as $story)
-                        <tr>
-                            <td>{{ $story->title }}</td>
-                            <td>{{ $story->type }}</td>
-                            <td>{{ $story->status == 1 ? 'Yes' : 'No' }}</td>
-                            <td>
-                                <a href="{{ route('stories.show', [$story]) }}" class="btn btn-sm btn-secondary">View</a>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Type</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($stories as $story)
+                  <tr>
+                    <td>{{ $story->title }}</td>
+                    <td>{{ $story->type }}</td>
+                    <td>{{ $story->status == 1 ? 'Yes' : 'No' }}</td>
+                    <td>
+                      <a href="{{ route('stories.show', [$story]) }}" class="btn btn-sm btn-secondary">View</a>
 
-                                <a href="{{ route('stories.edit', [$story]) }}" class="btn btn-sm btn-secondary">Edit</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
+                      <a href="{{ route('stories.edit', [$story]) }}" class="btn btn-sm btn-secondary">Edit</a>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
             </table>
 
             <div class="d-flex justify-content-end">
-                {{ $stories->links() }}
+              {{ $stories->links() }}
             </div>
           </div>
         </div>
