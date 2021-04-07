a18de9a8b8ed59ff94e63477f7908feead6c7832
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-07 14:35:00 +0100

    add stories link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('stories.index')" :active="request()->routeIs('stories.index')">
+                        {{ __('Stories') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -65,6 +71,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('stories.index')" :active="request()->routeIs('stories.index')">
+                {{ __('Stories') }}
             </x-responsive-nav-link>
         </div>
 
