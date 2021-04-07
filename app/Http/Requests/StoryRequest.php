76a85ce31ccea478eba01f2e35d7caabf52e2988
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-07 08:12:04 +0100

    define validation rules & custom error messages

--- app/Http/Requests/StoryRequest.php
+++ app/Http/Requests/StoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoryRequest extends FormRequest
@@ -23,11 +24,42 @@ class StoryRequest extends FormRequest
      */
     public function rules()
     {
+        /**
+         * ? Get story Id to ignore unique titles when updating
+         */
+        $storyID = $this->route('story.id');
         return [
-            'title' => 'required',
-            'body' => 'required',
+            'title' => [
+                'required', 'min:10', 'max:50',
+                function ($attribue, $value, $fail) {
+                    if ($value == 'Dummy Title') {
+                        $fail($attribue . ' is not valid');
+                    }
+                },
+                Rule::unique('stories')->ignore($storyID)
+            ],
+            'body' => 'required|min:50',
             'type' => 'required',
             'status' => 'required',
+        ];
+    }
+
+    public function withValidator($v)
+    {
+        $v->sometimes(
+            'body',
+            'max:200',
+            function ($input) {
+                return 'short' == $input->type;
+            }
+        );
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'You must enter :attribute',
+            'required' => 'Please enter :attribute',
         ];
     }
 }
