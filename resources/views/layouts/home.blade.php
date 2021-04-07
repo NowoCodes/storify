e5a32e6853148f147dd473555267668b1b06c336
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-07 13:42:28 +0100

    include nav

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -23,19 +23,7 @@
 <body class="font-sans antialiased">
   <!-- Page Content -->
   <main class="container pt-5">
-    @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
+    @include('layouts.navigation')
 
     @if (session('status'))
       <div class="alert alert-success alert-dismissible fade show" role="alert">
